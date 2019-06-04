3a501b17b42800de04fe293279827f6a217df975
BinaryWorlds <36459815+BinaryWorlds@users.noreply.github.com>
2019-06-04 23:48:41 +0200

    Update TPrinter.cpp

--- TPrinter.cpp
+++ TPrinter.cpp
@@ -1,6 +1,6 @@
 /* made by BinaryWorlds
 Not for commercial use,
-in other case by free to use it.
+in other case by free to use it. Just copy this text and link to oryginal repository: https://github.com/BinaryWorlds/ThermalPrinter
 
 I am not responsible for errors in the library. I deliver it "as it is".
 I will be grateful for all suggestions.
