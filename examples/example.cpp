4f1908abbdcd879be95350ec88b49f12eadc0e49
BinaryWorlds <36459815+BinaryWorlds@users.noreply.github.com>
2019-06-06 13:07:08 +0200

    Update example.cpp

--- examples/example.cpp
+++ examples/example.cpp
@@ -1,3 +1,9 @@
+/* made by BinaryWorlds
+Not for commercial use,
+in other case by free to use it. Just copy this text and link to oryginal repository: https://github.com/BinaryWorlds/ThermalPrinter
+I am not responsible for errors in the library. I deliver it "as it is".
+I will be grateful for all suggestions.
+*/
 #include <Arduino.h>
 #include "TPrinter.h"
 #include <HardwareSerial.h>
@@ -8,43 +14,42 @@ HardwareSerial mySerial(1);
 pin 17 Tx
 pin 16 Rx
 */
-Tprinter druk(&mySerial, 9600);
+Tprinter myPrinter(&mySerial, 9600);
 
 void setup() {
   micros();
   Serial.begin(9600); // monitor
   mySerial.begin(9600, SERIAL_8N1, 16, 17);
+  myPrinter.begin();
+  myPrinter.enableDtr(4);
 
-  druk.begin();
-
-  druk.identifyChars("ą ę"); // UTF-8
-  druk.printCharset();
-  druk.printCodepage();
-  druk.justify('C');
-  druk.println("center");
-  druk.justify('R');
-  druk.println("right");
-  druk.justify('L');
-  druk.println("left");
+  myPrinter.identifyChars("ą ę"); // UTF-8
+  myPrinter.printCharset();
+  myPrinter.printCodepage();
+  myPrinter.justify('C');
+  myPrinter.println("center");
+  myPrinter.justify('R');
+  myPrinter.println("right");
+  myPrinter.justify('L');
+  myPrinter.println("left");
 
   uint8_t list[]={5,10,15,20,25};
-  druk.setTabs(list, 5);
-  druk.print("1");
-  druk.tab();
-  druk.print("2");
-  druk.tab();
-  druk.print("3");
-  druk.tab();
-  druk.print("4");
-  druk.println();
+  myPrinter.setTabs(list, 5);
+  myPrinter.print("1");
+  myPrinter.tab();
+  myPrinter.print("2");
+  myPrinter.tab();
+  myPrinter.print("3");
+  myPrinter.tab();
+  myPrinter.print("4");
+  myPrinter.println();
 
-  druk.setMode(FONT_B, DOUBLE_WIDTH, DOUBLE_HEIGHT);
-  druk.println("FONT_B, bigger");
-  druk.unsetMode(FONT_B);
-  druk.println("FONT_A, bigger");
-
+  myPrinter.setMode(FONT_B, DOUBLE_WIDTH, DOUBLE_HEIGHT);
+  myPrinter.println("FONT_B, bigger");
+  myPrinter.unsetMode(FONT_B);
+  myPrinter.println("FONT_A, bigger");
 }
 
 void loop() {
-  druk.printFromSerial(); //open monitor and print something
+  myPrinter.printFromSerial(); //open monitor and print something
 }
